727886ecec2982fa7ece2579e8ba600d1760e752
adrianjustdoit <chrisdyah73@gmail.com>
2025-11-28 08:15:57 +0700

    Register change

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,120 +1,196 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import GuestLayout from '@/Layouts/GuestLayout.jsx';
+import { useForm } from '@inertiajs/react';
+import InputLabel from '@/Components/InputLabel.jsx';
+import TextInput from '@/Components/TextInput.jsx';
+import InputError from '@/Components/InputError.jsx';
+import PrimaryButton from '@/Components/PrimaryButton.jsx';
 
-export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
+export default function RegisterSeller() {
+    const { data, setData, post, processing, errors } = useForm({
+        shop_name: '',
+        shop_description: '',
+        pic_name: '',
+        pic_phone: '',
+        pic_email: '',
+        pic_address: '',
+        rt: '',
+        rw: '',
+        kelurahan: '',
+        kecamatan: '',
+        kota: '',
+        provinsi: '',
+        pic_id_number: '',
+        pic_id_photo: null,
+        pic_photo: null,
     });
 
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation');
-        };
-    }, []);
-
-    const handleOnChange = (event) => {
-        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
-    };
-
-    const submit = (e) => {
+    function submit(e) {
         e.preventDefault();
-
-        post(route('register'));
-    };
+        post(route('seller.register.store'), {
+            forceFormData: true, // untuk upload file
+        });
+    }
 
     return (
         <GuestLayout>
-            <Head title="Register" />
+            <h1>Registrasi Penjual</h1>
 
-            <form onSubmit={submit}>
+            <form onSubmit={submit} encType="multipart/form-data" className="space-y-4">
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
+                    <InputLabel htmlFor="shop_name" value="Nama Toko" />
                     <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
+                        id="shop_name"
+                        value={data.shop_name}
+                        onChange={e => setData('shop_name', e.target.value)}
                         className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={handleOnChange}
-                        required
                     />
-
-                    <InputError message={errors.name} className="mt-2" />
+                    <InputError message={errors.shop_name} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
+                <div>
+                    <InputLabel htmlFor="shop_description" value="Deskripsi Toko" />
                     <TextInput
-                        id="email"
+                        id="shop_description"
+                        value={data.shop_description}
+                        onChange={e => setData('shop_description', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.shop_description} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_name" value="Nama PIC" />
+                    <TextInput
+                        id="pic_name"
+                        value={data.pic_name}
+                        onChange={e => setData('pic_name', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.pic_name} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_phone" value="Nomor Telepon PIC" />
+                    <TextInput
+                        id="pic_phone"
+                        value={data.pic_phone}
+                        onChange={e => setData('pic_phone', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.pic_phone} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_email" value="Email PIC" />
+                    <TextInput
+                        id="pic_email"
                         type="email"
-                        name="email"
-                        value={data.email}
+                        value={data.pic_email}
+                        onChange={e => setData('pic_email', e.target.value)}
                         className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={handleOnChange}
-                        required
                     />
-
-                    <InputError message={errors.email} className="mt-2" />
+                    <InputError message={errors.pic_email} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
+                <div>
+                    <InputLabel htmlFor="pic_address" value="Alamat PIC" />
                     <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
+                        id="pic_address"
+                        value={data.pic_address}
+                        onChange={e => setData('pic_address', e.target.value)}
                         className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={handleOnChange}
-                        required
                     />
-
-                    <InputError message={errors.password} className="mt-2" />
+                    <InputError message={errors.pic_address} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
-
+                <div>
+                    <InputLabel htmlFor="rt" value="RT" />
                     <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
+                        id="rt"
+                        value={data.rt}
+                        onChange={e => setData('rt', e.target.value)}
                         className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={handleOnChange}
-                        required
                     />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError message={errors.rt} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="rw" value="RW" />
+                    <TextInput
+                        id="rw"
+                        value={data.rw}
+                        onChange={e => setData('rw', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.rw} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="kelurahan" value="Kelurahan" />
+                    <TextInput
+                        id="kelurahan"
+                        value={data.kelurahan}
+                        onChange={e => setData('kelurahan', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.kelurahan} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="kecamatan" value="Kecamatan" />
+                    <TextInput
+                        id="kecamatan"
+                        value={data.kecamatan}
+                        onChange={e => setData('kecamatan', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.kelurahan} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="kota" value="Kota" />
+                    <TextInput
+                        id="kota"
+                        value={data.kota}
+                        onChange={e => setData('kota', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.kota} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="provinsi" value="Provinsi" />
+                    <TextInput
+                        id="provinsi"
+                        value={data.provinsi}
+                        onChange={e => setData('provinsi', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.provinsi} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_id_number" value="Nomor ID PIC" />
+                    <TextInput
+                        id="pic_id_number"
+                        value={data.pic_id_number}
+                        onChange={e => setData('pic_id_number', e.target.value)}
+                        className="mt-1 block w-full"
+                    />
+                    <InputError message={errors.pic_id_number} className="mt-2" />
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_id_photo" value="Foto KTP PIC" />
+                    <input
+                        id="pic_id_photo"
+                        type="file"
+                        onChange={e => setData('pic_id_photo', e.target.files[0])}
+                        className="mt-1 block w-full"
+                    />
+                    {errors.pic_id_photo && <div>{errors.pic_id_photo}</div>}
+                </div>
+                <div>
+                    <InputLabel htmlFor="pic_photo" value="Foto PIC" />
+                    <input
+                        id="pic_photo"
+                        type="file"
+                        onChange={e => setData('pic_photo', e.target.files[0])}
+                        className="mt-1 block w-full"
+                    />
+                    {errors.pic_photo && <div>{errors.pic_photo}</div>}
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
+                <PrimaryButton disabled={processing} className="mt-4">
+                    Kirim Registrasi
+                </PrimaryButton>
             </form>
         </GuestLayout>
     );
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,106 +1,214 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { useForm } from '@inertiajs/vue3';
 
 const form = useForm({
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
-    terms: false,
+  shop_name: '',
+  shop_description: '',
+  pic_name: '',
+  pic_phone: '',
+  pic_email: '',
+  pic_address: '',
+  rt: '',
+  rw: '',
+  kelurahan: '',
+  kota: '',
+  provinsi: '',
+  pic_id_number: '',
+  pic_id_photo: null,
+  pic_photo: null,
 });
 
 const submit = () => {
-    form.post(route('register'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
+  form.post(route('seller.register.store'), {
+    forceFormData: true,
+  });
 };
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Register" />
+  <GuestLayout>
+    <h1 class="text-xl font-bold mb-4">Registrasi Penjual</h1>
 
-        <h1 class="text-3xl font-bold text-center text-sky-700 mb-6">Lapak Mahasiswa</h1>
+    <form @submit.prevent="submit" enctype="multipart/form-data" class="space-y-4">
+      <div>
+        <label class="block text-sm font-medium">Nama Toko</label>
+        <input
+          v-model="form.shop_name"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.shop_name" class="text-red-600 text-sm">
+          {{ form.errors.shop_name }}
+        </div>
+      </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Name" />
+      <div>
+        <label class="block text-sm font-medium">Deskripsi Toko</label>
+        <input
+          v-model="form.shop_description"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.shop_description" class="text-red-600 text-sm">
+          {{ form.errors.shop_description }}
+        </div>
+      </div>
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+      <div>
+        <label class="block text-sm font-medium">Nama PIC</label>
+        <input
+          v-model="form.pic_name"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.pic_name" class="text-red-600 text-sm">
+          {{ form.errors.pic_name }}
+        </div>
+      </div>
 
-                <InputError class="mt-2" :message="form.errors.name" />
-            </div>
+      <div>
+        <label class="block text-sm font-medium">Nomor Telepon PIC</label>
+        <input
+          v-model="form.pic_phone"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.pic_phone" class="text-red-600 text-sm">
+          {{ form.errors.pic_phone }}
+        </div>
+      </div>
 
-            <div class="mt-4">
-                <InputLabel for="email" value="Email" />
+      <div>
+        <label class="block text-sm font-medium">Email PIC</label>
+        <input
+          type="email"
+          v-model="form.pic_email"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.pic_email" class="text-red-600 text-sm">
+          {{ form.errors.pic_email }}
+        </div>
+      </div>
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
+      <div>
+        <label class="block text-sm font-medium">Alamat PIC</label>
+        <input
+          v-model="form.pic_address"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.pic_address" class="text-red-600 text-sm">
+          {{ form.errors.pic_address }}
+        </div>
+      </div>
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+      <div class="grid grid-cols-2 gap-2">
+        <div>
+          <label class="block text-sm font-medium">RT</label>
+          <input
+            v-model="form.rt"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.rt" class="text-red-600 text-sm">
+            {{ form.errors.rt }}
+          </div>
+        </div>
+        <div>
+          <label class="block text-sm font-medium">RW</label>
+          <input
+            v-model="form.rw"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.rw" class="text-red-600 text-sm">
+            {{ form.errors.rw }}
+          </div>
+        </div>
+        
+      </div>
+      <div class="grid grid-cols-2 gap-2">
+        <div>
+          <label class="block text-sm font-medium">Kelurahan</label>
+          <input
+            v-model="form.kelurahan"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.kelurahan" class="text-red-600 text-sm">
+            {{ form.errors.kelurahan }}
+          </div>
+        </div>
+        <div>
+          <label class="block text-sm font-medium">Kecamatan</label>
+          <input
+            v-model="form.kecamatan"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.kecamatan" class="text-red-600 text-sm">
+            {{ form.errors.kecamatan }}
+          </div>
+        </div>
+      </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+      <div class="grid grid-cols-2 gap-2">
+        <div>
+          <label class="block text-sm font-medium">Kota</label>
+          <input
+            v-model="form.kota"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.kota" class="text-red-600 text-sm">
+            {{ form.errors.kota }}
+          </div>
+        </div>
+        <div>
+          <label class="block text-sm font-medium">Provinsi</label>
+          <input
+            v-model="form.provinsi"
+            class="mt-1 block w-full border rounded px-2 py-1"
+          />
+          <div v-if="form.errors.provinsi" class="text-red-600 text-sm">
+            {{ form.errors.provinsi }}
+          </div>
+        </div>
+      </div>
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
+      <div>
+        <label class="block text-sm font-medium">Nomor ID PIC</label>
+        <input
+          v-model="form.pic_id_number"
+          class="mt-1 block w-full border rounded px-2 py-1"
+        />
+        <div v-if="form.errors.pic_id_number" class="text-red-600 text-sm">
+          {{ form.errors.pic_id_number }}
+        </div>
+      </div>
 
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+      <div>
+        <label class="block text-sm font-medium">Foto KTP PIC</label>
+        <input
+          type="file"
+          class="mt-1 block w-full"
+          @change="e => form.pic_id_photo = e.target.files[0]"
+        />
+        <div v-if="form.errors.pic_id_photo" class="text-red-600 text-sm">
+          {{ form.errors.pic_id_photo }}
+        </div>
+      </div>
 
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
+      <div>
+        <label class="block text-sm font-medium">Foto PIC</label>
+        <input
+          type="file"
+          class="mt-1 block w-full"
+          @change="e => form.pic_photo = e.target.files[0]"
+        />
+        <div v-if="form.errors.pic_photo" class="text-red-600 text-sm">
+          {{ form.errors.pic_photo }}
+        </div>
+      </div>
 
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-sky-700 hover:text-sky-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+      <button
+        type="submit"
+        :disabled="form.processing"
+        class="px-4 py-2 bg-indigo-600 text-white rounded disabled:opacity-50"
+      >
+        Kirim Registrasi
+      </button>
+    </form>
+  </GuestLayout>
 </template>
